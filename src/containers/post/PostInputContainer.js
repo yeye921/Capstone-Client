bdeaf43f75b7940a1d2fcbadb4de9feb5b5b1623
ByeonHajin <gkwls1130@ajou.ac.kr>
2022-04-26 00:24:27 +0900

    REFACTOR: 나눔 게시글 등록 파일명 수정 PostViewerContainer --> PostInputContainer

---
+++ src/containers/post/PostInputContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PostInput from "../../components/post/PostInput/PostInput";
+
+const PostInputContainer = () => {
+    return <PostInput />;
+}
+
+export default PostInputContainer;
--- src/containers/post/PostViewerContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PostInput from "../../components/post/PostViewer/PostViewer";
-
-const PostViewerContainer = () => {
-    return <PostInput />;
-}
-
-export default PostViewerContainer;
