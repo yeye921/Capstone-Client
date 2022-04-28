30970d7f59c3e8193a427bdf197cbcd92640b15c
ByeonHajin <gkwls1130@ajou.ac.kr>
2022-04-28 23:28:27 +0900

    BUILD: vercel 배포 https,http 프로토콜 문제 롤백

--- src/services/post/index.js
+++ src/services/post/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const POST_BASE_URL = "https://3.39.125.17/post"
+const POST_BASE_URL = "http://3.39.125.17/post"
 
 export const postData = async(inputData, postTime) => {
     const headers = {
