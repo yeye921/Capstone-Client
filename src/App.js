4e7fd11f878b492c47e99e703fcf9862232b9e6a
ByeonHajin <gkwls1130@ajou.ac.kr>
2022-05-02 18:54:10 +0900

    FEAT: 모집 마감 테스트 페이지

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Login from './pages/Login';
 import Sign from './pages/Sign';
 import Detail from './pages/Detail';
 import Here from './pages/Here';
+import Detail from './pages/Detail'
+import Test from './pages/Test';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Sign />} />
-        <Route path="/main/detail" element={<Detail />} />
-        <Route path="/main/*" element={<Main />} />
-        <Route path="/post" element={<Post />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/here" element={<Here />} />
-      </Routes>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Sign />} />
+            <Route path="/main/detail" element={<Detail />} />
+            <Route path="/main/*" element={<Main />} />
+            <Route path="/post" element={<Post />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/here" element={<Here />} />
+            <Route path="/test" element={<Test />}/>
+        </Routes>
     </Router>
   );
 }
