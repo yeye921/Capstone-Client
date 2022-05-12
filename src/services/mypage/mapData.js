f4c973259c73c3668aa22dd1b2192411f40105f8
Yewon Kim01~ <yewon921@ajou.ac.kr>
2022-05-12 15:32:09 +0900

    FEAT: 여기서 모여 페이지 구현

---
+++ src/services/mypage/mapData.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://3.39.125.17/mymap';
+
+export const mapData = async (input, roadAddr, lotAddr, x, y) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+  };
+  try {
+    const response = await axios.post(
+      BASE_URL,
+      {
+        u_id: 1,
+        pl_name: input,
+        x: x,
+        y: y,
+        road_address: roadAddr,
+        lot_address: lotAddr,
+      },
+      { headers: headers },
+      { withCredentials: false },
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
