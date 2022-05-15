43e874ad8aef5d3a56bf240cb68d15c0ffc20dd2
Yewon Kim01~ <yewon921@ajou.ac.kr>
2022-05-15 22:44:07 +0900

    FEAT: 사용자 정보 recoil에 저장

--- src/services/sign/signData.js
+++ src/services/sign/signData.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const POST_BASE_URL = 'http://3.39.125.17/signup';
+const POST_BASE_URL = "http://3.39.125.17/signup";
 //윤정ip
 
 export const signData = async (inputData) => {
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
   try {
     const response = await axios.put(
@@ -20,7 +20,8 @@ export const signData = async (inputData) => {
       { headers: headers },
       { withCredentials: false },
     );
-    console.log(response);
+    // console.log("서버", response.data);
+    return response.data;
   } catch (error) {
     console.error(error);
   }
--- src/state.js
+++ src/state.js
@@ -1,7 +1,67 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist();
 
-// recoil로 관리할 변수들
+// recoil로 관리할 변수들..
+// uId
 export const idState = atom({
-  key: 'idState',
-  default: '',
+  key: "idState",
+  default: 0,
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 지번 주소
+export const addrState = atom({
+  key: "addrState",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 도로명 주소
+export const roadAddrState = atom({
+  key: "roadAddrState",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
+
+// x좌표
+export const xState = atom({
+  key: "xState",
+  default: 0,
+  effects_UNSTABLE: [persistAtom],
+});
+
+// y좌표
+export const yState = atom({
+  key: "yState",
+  default: 0,
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 은행
+export const bankState = atom({
+  key: "bankState",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 계좌번호
+export const accountState = atom({
+  key: "accountState",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 닉네임
+export const nameState = atom({
+  key: "nameState",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 포인트
+export const pointState = atom({
+  key: "pointState",
+  default: 0,
+  effects_UNSTABLE: [persistAtom],
 });
