004445e9bfbb730cfa88514b19e22effe4f8b6cd
ByeonHajin <gkwls1130@ajou.ac.kr>
2022-05-20 00:52:54 +0900

    FIX: 채팅 디자인 수정

--- src/components/chat/firebase/ChatInput.js
+++ src/components/chat/firebase/ChatInput.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { db } from "./firebase";
@@ -8,89 +8,84 @@ import Message from "./Message";
 import styled from "styled-components";
 
 const Input = styled.input`
-    width: 18rem;
-    height: 3rem;
-    border:none;
-    border-right:0px;
-    border-top:0px;
-    boder-left:0px;
-    boder-bottom:0px;
-    padding-left: 1rem;
-    font-size: 15px;
-    outline: none;
-
+  width: 18rem;
+  height: 3rem;
+  border: none;
+  border-right: 0px;
+  border-top: 0px;
+  boder-left: 0px;
+  boder-bottom: 0px;
+  padding-left: 1rem;
+  font-size: 15px;
+  outline: none;
 `;
 const Button = styled.button`
-    width: 5rem;
-    height: 4rem;
-    margin-right: 0.5rem;
-    margin-top: 0.5rem;
+  width: 5rem;
+  height: 4rem;
+  margin-right: 0.5rem;
+  margin-top: 0.5rem;
 `;
 const Div = styled.div`
-    border: 1px solid grey;
-    height: 5rem;
-    display: flex;
-    border-radius: 0.2rem;
-    // border-width: 0.1rem;
+  border: 1px solid grey;
+  height: 5rem;
+  display: flex;
+  border-radius: 0.2rem;
+  // border-width: 0.1rem;
 
-    // position: fixed;
-    // position : relative;
-    // transform : translateY(-100%);
+  // position: fixed;
+  // position : relative;
+  // transform : translateY(-100%);
 
-    background-color: white;
-    // margin : 0.2rem;
+  background-color: white;
+  // margin : 0.2rem;
 
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 `;
 
-
 const ChatInput = () => {
-    const [username, setUSername] = useRecoilState(nameState); 
-    const [room, setRoom] = useRecoilState(pidState); // 실제 
+  const [username, setUSername] = useRecoilState(nameState);
+  const [room, setRoom] = useRecoilState(pidState); // 실제
 
-    const [input, setInput] = useState("");
-    const [msgs, setMsgs] = useState([]);
+  const [input, setInput] = useState("");
+  const [msgs, setMsgs] = useState([]);
 
-    // 없애기
-    // const [username, setUsername] = useState('');
-    // const [room, setRoom] = useState("1");  // 예시
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    // useEffect(() => {
-    //     setUsername(prompt('이름을 넣으세요~'))
-    //   }, [])
-  
+  const onSending = (e) => {
+    e.preventDefault();
+    db.collection(room).add({
+      msg: input,
+      username: username, // uId로 설정
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      // timestamp: Date.now(),
+    });
+    setMsgs([...msgs, { username: username, msg: input }]);
+    setInput("");
+  };
 
-    const onSubmit = (e) => {
-      e.preventDefault();
-    };
-  
-    const onSending = (e) => {
-      e.preventDefault();
-      db.collection(`${room}`).add({
-        msg: input,
-        username: username, // uId로 설정
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        // timestamp: Date.now(),
-      });
-      setMsgs([...msgs, { username: username, msg: input }]);
-      setInput("");
-    };
-
-    return(
-        <form onSubmit={onSubmit}>
-        <Div>
-          <Input value={input} placeholder="내용을 입력해주세요" onChange={(e) => setInput(e.target.value)} />
-          <Button
-            disabled={!input}
-            variant="contained"
-            color="primary"
-            type="submit"
-            onClick={onSending}
-          >
-            전송
-          </Button>
-        </Div>
-      </form>
-
-    )
-}
+  return (
+    <form onSubmit={onSubmit}>
+      <Div>
+        <Input
+          value={input}
+          placeholder="내용을 입력해주세요"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={onSending}
+        >
+          전송
+        </Button>
+      </Div>
+    </form>
+  );
+};
 export default ChatInput;
--- src/containers/chat/ChatContainer.js
+++ src/containers/chat/ChatContainer.js
@@ -11,7 +11,15 @@ import axios from "axios";
 import { useHere } from "../../services/mutation";
 import NoticeBar from "../../components/chat/NoticeBar";
 import { useRecoilState } from "recoil";
-import { idState, roadAddrState, addrState, pidState, titleState, feeState, nameState } from "../../state";
+import {
+  idState,
+  roadAddrState,
+  addrState,
+  pidState,
+  titleState,
+  feeState,
+  nameState,
+} from "../../state";
 import styled from "styled-components";
 
 import ChatContent from "../../components/chat/firebase/ChatContent";
@@ -22,9 +30,14 @@ import "firebase/firestore";
 import { db } from "../../components/chat/firebase/firebase";
 
 const ButtonContainer = styled.div`
-  display: flex;
-  height: 3em;
-  justify-content: center;
+  display: grid;
+  justify-content: start;
+
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1.25fr;
 `;
 
 const ChatContainer = ({ state }) => {
@@ -45,18 +58,16 @@ const ChatContainer = ({ state }) => {
   const here = useHere("here", "http://3.39.125.17/chat");
   const { data } = useQuery("here");
 
-
   const [place, setPlace] = useState(""); // 상단 바 나눔 위치
   const [cnt, setCnt] = useState(1);
 
   // 원래 주석처리 됨
   if (data) {
     console.log("data: ", data);
-    console.log("1",data.place_name);
+    console.log("1", data.place_name);
 
     // 이거 맞는지?
     // sendingHere(data.place_name);
-    
 
     // setPostInfo({
     //   ...postInfo,
@@ -65,7 +76,6 @@ const ChatContainer = ({ state }) => {
     //     location: data.place_name,
     //   },
     // });
-
   }
 
   const onClosing = (e) => {
@@ -106,10 +116,10 @@ const ChatContainer = ({ state }) => {
 
     const msg = "내가 쏜다!";
     console.log(roadAddr);
-    if(roadAddr.length == 0){
-      sendingSSonda(msg, addr); 
+    if (roadAddr.length == 0) {
+      sendingSSonda(msg, addr);
     } else {
-      sendingSSonda(msg, roadAddr); 
+      sendingSSonda(msg, roadAddr);
     }
   };
 
@@ -122,7 +132,6 @@ const ChatContainer = ({ state }) => {
     });
   };
 
-
   const sendingSSonda = (msg, place) => {
     db.collection(`${pId}`).doc("ssonda").set({
       msg: msg,
@@ -131,7 +140,7 @@ const ChatContainer = ({ state }) => {
       place: place,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
-  }
+  };
 
   const sendingClose = (msg) => {
     db.collection(`${pId}`).doc("close").set({
@@ -140,13 +149,13 @@ const ChatContainer = ({ state }) => {
       type: "close",
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
-  }
+  };
 
   const sendingHere = (place) => {
     db.collection(`${pId}`).doc("place").set({
       place: place,
-    })
-  }
+    });
+  };
 
   // MenuModal에서 받아온 데이터 recoil로 저장
   useEffect(() => {
@@ -154,32 +163,31 @@ const ChatContainer = ({ state }) => {
     setPid(state.pId);
     setTitle(state.title);
     setFee(state.fee);
-    console.log("recoil",pId, title, fee);
+    console.log("recoil", pId, title, fee);
     // console.log("fee",fee);
     // setPostFee(state.fee)
-  }, [])
-
+  }, []);
 
   useEffect(() => {
     db.collection(`${pId}`)
       .orderBy("timestamp")
       .onSnapshot((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          if(doc.data().type == "close"){
+          if (doc.data().type == "close") {
             console.log("모집마감 주체자", doc.data().username);
             setCBtn(true);
           }
-          if(doc.data().type == "ssonda"){
+          if (doc.data().type == "ssonda") {
             console.log("내가쏜다 주체자", doc.data().username);
             setSBtn(true);
             setPlace(doc.data().place);
             setFee("0");
           }
-          if(doc.data().username == "관리자"){
-            console.log("db에 존재", cnt, fee)
-            setCnt(cnt+1);
-            setFee(fee/cnt); // 둘다 int형이어야 함
-            console.log("db에 존재2", cnt, fee)
+          if (doc.data().username == "관리자") {
+            console.log("db에 존재", cnt, fee);
+            setCnt(cnt + 1);
+            setFee(fee / cnt); // 둘다 int형이어야 함
+            console.log("db에 존재2", cnt, fee);
           }
         });
       });
@@ -197,41 +205,29 @@ const ChatContainer = ({ state }) => {
   //     })
   // })
 
-
   return (
     <div>
       {here.isLoading && <div>isLoading</div>}
-      <NoticeBar
-        fee={fee}
-        addr={place}
-      />
+      <NoticeBar fee={fee} addr={place} />
 
       <ChatContent />
 
       <ButtonContainer>
-        <Button
-          id="closeBtn"
-          onClick={onClosing}
-          disabled={closeBtn}
-        >
+        <Button id="closeBtn" onClick={onClosing} disabled={closeBtn}>
           모집마감
         </Button>
 
-        <IconButton
-          onClick={onShooting}
-          disabled={shootBtn}
-        >
+        <IconButton onClick={onShooting} disabled={shootBtn}>
           <MonetizationOnIcon
             sx={{
               fontSize: 50,
             }}
           />
         </IconButton>
-        
-        <Button onClick={onMap}>지도</Button>
-      </ButtonContainer>
 
-      <ChatInput />
+        <Button onClick={onMap}>지도</Button>
+        <ChatInput />
+      </ButtonContainer>
     </div>
   );
 };
