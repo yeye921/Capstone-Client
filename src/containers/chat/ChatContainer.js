6b6ba7ed2487b991669e26c1447040516f25e56b
Yewon Kim01~ <yewon921@ajou.ac.kr>
2022-05-25 18:10:23 +0900

    FIX: 채팅리스트-> 채팅방 오류 해결

--- src/containers/chat/ChatContainer.js
+++ src/containers/chat/ChatContainer.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from "../../components/common/Button";
-import { ssondaData } from "../../services/chat";
+import { getOrderData, ssondaData } from "../../services/chat";
 import GroupsIcon from '@mui/icons-material/Groups';
-import MapIcon from '@mui/icons-material/Map';
 import { FaMapMarkedAlt } from 'react-icons/fa';
 import {  IoReceipt } from 'react-icons/io5';
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
@@ -39,7 +38,7 @@ const ButtonContainer = styled.div`
 
 const ChatContainer = ({ state }) => {
   const [name, setName] = useRecoilState(nameState);
-  const [fee, setFee] = useRecoilState(feeState); // 상단 바 배달비
+  // const [fee, setFee] = useRecoilState(feeState); // 상단 바 배달비
   const [uId, setuId] = useRecoilState(idState);
   const [roadAddr, setRoadAddr] = useRecoilState(roadAddrState);
   const [addr, setAddr] = useRecoilState(addrState);
@@ -54,6 +53,13 @@ const ChatContainer = ({ state }) => {
     isOpen: false,
   });
 
+  const [totalFee, setTotalFee] = useState(state.fee);
+  const [fee, setFee] = useState(state.fee); // 상단 바 배달비
+
+  // state.fee로 다 수정하기
+  // const [totalFee, setTotalFee] = useState("3000");
+  // let [fee, setFee] = useState("3000"); // 상단 바 배달비
+
   const navigate = useNavigate();
 
   //const { pId } = queryString.parse(state);
@@ -62,10 +68,12 @@ const ChatContainer = ({ state }) => {
 
 
   const [place, setPlace] = useState(""); // 상단 바 나눔 위치
-  // const [cnt, setCnt] = useState(1);
+  const [click, setClick] = useState(false); // 내가 쏜다가 클릭되었는지 여부
+  const [ssonda, setSssonda] = useState(false); // 내가 쏜다 주체자인지 여부
+
 
   const sendingHere = (place) => {
-    const room = pId + "_1";
+    const room = pId + "나눔 위치";
     db.collection(`${room}`).add({
       place: place,
       username: "여기서모여위치",
@@ -74,57 +82,34 @@ const ChatContainer = ({ state }) => {
 
   // 원래 주석처리 됨
   if (data) {
-    // console.log("data: ", data);
-    console.log("1",data.place_name);
+    // console.log("서버에서 받아온 나눔위치",data.place_name);
 
-    // 이거 맞는지?
     sendingHere(data.place_name);
   }
 
   // useEffect(()=> {
   //   if(data) {
-  //     console.log("1",data.place_name);
-  //     // 이거 맞는지?
   //     sendingHere(data.place_name);
   //     // 이걸 보내고 나서 읽어와야함
   //   }
   // }, [data]);
 
-  const onClosing = (e) => {
+  const onClosing = () => {
     const msg = "모집이 마감되었습니다.";
     sendingClose(msg);
     here.mutate({ pId: pId });
-
-    // console.log("2",data.place_name);
-    // sendingHere(data.place_name);
   };
 
-  const onShooting = (e) => {
-    // ssondaData(uId, pId).then((response) => {
-    //   // let fee = 0;
-    //   // if (uId == response.data.uId) {
-    //   //   // setFee(response.data.total_fee);
-    //   //   fee = response.data.total_fee;
-    //   // }
-
+  const onShooting = () => {
     ssondaData(uId, pId);
 
-    const msg = "내가 쏜다!";
+    const msg = name +"님이 쏜다! 내 위치로 집합!";
     console.log(roadAddr);
     if(roadAddr.length === 0){
       sendingSSonda(msg, addr); 
     } else {
       sendingSSonda(msg, roadAddr); 
     }
-  };
-
-  const onMap = (e) => {
-    navigate("/here", {
-      state: {
-        pId: pId,
-        title: title,
-      },
-    });
   };
 
   const sendingSSonda = (msg, place) => {
@@ -146,23 +131,29 @@ const ChatContainer = ({ state }) => {
     });
   }
 
+  const onMap = (e) => {
+    navigate("/here", {
+      state: {
+        pId: pId,
+        title: title,
+      },
+    });
+  };
 
   // MenuModal에서 받아온 데이터 recoil로 저장
   // 여기서 두 콘솔 다르게 찍힘
-  // useEffect(() => {
-  //   console.log(state.pId, state.title, state.fee);
-  //   setPid(state.pId);
-  //   setTitle(state.title);
-  //   // setFee(state.fee);
-  //   console.log("recoil",pId, title, fee);
-  //   // console.log("fee",fee);
-  //   // setPostFee(state.fee)
-  // }, [])
 
   // 맨 처음 배달비 설정
-  useEffect(()=> {
-    setFee(state.fee);
-  },[]);
+  // useEffect(()=> {
+  //   setFee(state.fee);
+  //   setTotalFee(state.fee);
+  //   console.log("이전페이지에서 받아온 fee", state.fee, fee, totalFee);
+  // },[]);
+
+  // useEffect(() => {
+  //   const fee = state.fee;
+  //   console.log("이전에서 넘어온 state fee", state.fee);
+  // }, [fee])
 
   // 채팅 받아오기
   useEffect(() => {
@@ -172,68 +163,77 @@ const ChatContainer = ({ state }) => {
       .orderBy("timestamp")
       .onSnapshot((querySnapshot) => {
         querySnapshot.forEach((doc) => {
+          if(doc.data().type === "ssonda"){
+            // console.log("ssoda클릭", click);
+            if(doc.data().username === name){
+              console.log("나는 내가쏜다 주체자입니다");
+              setFee(totalFee);
+              setSssonda(true);
+              console.log("fee", fee);
+            } else {
+              setFee(0);
+              console.log("fee", fee);
+            }
+            setClick(true); // 이게 적용 안됨
+            // click = "true";
+            setSBtn(true);
+            setPlace(doc.data().place);
+            // console.log("ssondaClicked", click);
+          }
           if(doc.data().type === "close"){
             console.log("모집마감 주체자", doc.data().username);
             setCBtn(true);
           }
-          if(doc.data().type === "ssonda"){
-            console.log("내가쏜다 주체자", doc.data().username);
-            setSBtn(true);
-            setPlace(doc.data().place);
-            setFee("0");
-          }
-          // if(doc.data().username === "관리자"){
-          //   console.log("db에 존재", cnt, currentFee)
-          //   cnt += 1;
-          //   currentFee /= cnt;  // 둘다 int형이어야 함
-          //   console.log("db에 존재2", cnt, currentFee)
-          // }
         });
       });
   }, []);
 
 
-  // Ver1, 일단 사람 수만 셈
-  // useEffect(() => {
-  //   let cnt = 0;
-  //   const room = pId + "_";
-  //   db.collection(`${room}`)
-  //     .orderBy("timestamp")
-  //     .onSnapshot((d) => {
-  //       d.forEach((doc) => {
-  //         console.log("문서개수");
-  //         if(doc.data().username === "관리자"){
-  //           console.log("db에 존재");
-  //           cnt += 1;
-  //           console.log("현재cnt",cnt);
-  //           // currentFee /= cnt;  // 둘다 int형이어야 함
-  //           // console.log("db에 존재2", cnt, currentFee)
-  //         }
-  //       })
-  //     })
-  // }, [])
-
   // Ver2 배달비 계산
   useEffect(() => {
-    // let cnt = 1;
-    // let currentFee = fee;  // 이런식으로 해야함
-    const room = pId + "_";
-    db.collection(`${room}`)
-      .orderBy("timestamp")
-      .onSnapshot((querySnapshot) => { // 변화 감지
-        let cnt = 1;
-        let currentFee = state.fee;
-        querySnapshot.forEach(doc => { // 각 문서마다 실행
-          if(doc.data().username === "관리자"){
-            // console.log("db에 존재", cnt, fee )
-            cnt += 1;
-            currentFee = Math.floor(currentFee/cnt);
-            setFee(currentFee);
-            console.log("db에 존재2", cnt, currentFee, fee);
-          }
+    // click = true;
+    console.log("배달비_쏜다클릭여부", click);
+    if(!click) {     // 내가 쏜다 안할 때만 배달비 계산함
+      const room = pId + "참여자 리스트";
+      db.collection(`${room}`)
+        .orderBy("timestamp")
+        .onSnapshot((querySnapshot) => { // 변화 감지
+          let cnt = 1;
+          console.log("totalFee", totalFee);
+          querySnapshot.forEach(doc => { // 각 문서마다 실행
+            if(doc.data().username === "관리자"){
+              cnt += 1;
+              let currentFee = Math.floor(totalFee/cnt);
+              setFee(currentFee);
+              // console.log("배달비 계산", cnt, currentFee, fee);
+            }
+          });
+        })
+      } else {
+        // 내가 쏜다일 경우
+        // 다시 원래대로 되돌림
+        if(ssonda){
+          setFee(totalFee);
+        } else {
+          setFee(0);
+        }
+      }
+  }, [click]) //click,ssonda 바꿔보기
+
+  
+  useEffect(() => {
+    const room = pId + "나눔 위치";
+    db.collection(`${room}`).where("username", "==", "여기서모여위치")
+    .onSnapshot((querySnapshot) => {
+      let loc = "";
+        querySnapshot.forEach((doc) => {
+            loc = doc.data().place;
+            setPlace(loc);
+            // console.log("둘이 똑같은 위치 찍혀야 함", loc, place);
         });
-      })
-  }, [])
+          //console.log
+      });
+  }, []);
 
   const onOrder = () => {
     getOrderData(pId).then((data) => {
@@ -283,20 +283,26 @@ const ChatContainer = ({ state }) => {
   //       })
   //     })
   // }, []);
-  useEffect(() => {
-    const room = pId + "_1";
-    db.collection(`${room}`).where("username", "==", "여기서모여위치")
-    .onSnapshot((querySnapshot) => {
-      let loc = "";
-        querySnapshot.forEach((doc) => {
-            // cities.push(doc.data().name);
-            loc = doc.data().place;
-            setPlace(loc);
-            console.log("loc&place", loc, place);
-        });
-        //console.log
-    });
-  }, []);
+  
+  // Ver1, 일단 사람 수만 셈
+  // useEffect(() => {
+  //   let cnt = 0;
+  //   const room = pId + "_";
+  //   db.collection(`${room}`)
+  //     .orderBy("timestamp")
+  //     .onSnapshot((d) => {
+  //       d.forEach((doc) => {
+  //         console.log("문서개수");
+  //         if(doc.data().username === "관리자"){
+  //           console.log("db에 존재");
+  //           cnt += 1;
+  //           console.log("현재cnt",cnt);
+  //           // currentFee /= cnt;  // 둘다 int형이어야 함
+  //           // console.log("db에 존재2", cnt, currentFee)
+  //         }
+  //       })
+  //     })
+  // }, [])
 
 
   return (
@@ -343,7 +349,7 @@ const ChatContainer = ({ state }) => {
         </IconButton>
         
       
-          <IconButton>
+          <IconButton onClick={onOrder}>
           {/* <div style={{display:"flex"}}> */}
             <IoReceipt
                   style={{color: "rgba(31, 122, 19, 0.8)", fontSize:"40"}}
